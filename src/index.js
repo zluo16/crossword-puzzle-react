f644dff76d032757d23b3f9ff2ad173721ff4343
Mike <zluo@alumni.berklee.edu>
2017-12-18 19:38:34 -0500

    removed redux dev tools from create store (breaks app if it doesn't detect the redux dev tools extension)

--- src/index.js
+++ src/index.js
@@ -8,10 +8,7 @@ import App from './App';
 
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(thunk))
 )
 
 ReactDOM.render(
